8791c16381b4101ee448d3bfccb281f3705e8808
iwantobelieve <44407611+iwantobelieve@users.noreply.github.com>
2018-10-26 15:54:13 -0600

    Update data_average.py

--- data_average.py
+++ data_average.py
@@ -8,7 +8,6 @@ from numpy import*
 
 s1 = loadtxt("scan1.txt")
 y1 = s1[:,1]
-#x1 = s1[:,0]
 
 s2 = loadtxt("scan2.txt")
 y2 = s2[:,1]
@@ -26,15 +25,12 @@ qtz = loadtxt("../../qtz_avg.txt")
 
 total=4
 length=s1.shape[1]
-#array=np.zeros(shape=(length,2))
-#x_corr=np.xeros(shape=(length,1))
 
 #x_corr=(1/((((1/x1)-(1/800))^-1))*10^-9)/100
 y1f=(y1-yb)/qtz #n
 y2f=(y2-yb)/qtz #n
 y3f=(y3-yb)/qtz #n
 y4f=(y4-yb)/qtz #n
-#avg_a=(y1f+y2f+y3f+y4f)/total #n
 
 avg=(y1+y2+y3+y4)/total
 bk_sub=avg-yb
